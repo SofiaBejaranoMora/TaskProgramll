483ab6e9feabc8ed3bace2d6b78de430c96dd013
SofiaBejaranoMora <142953533+SofiaBejaranoMora@users.noreply.github.com>
2025-03-06 23:28:13 -0600

    Arreglar error
    No puse la versión de java

--- TaskProgramll/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module cr.ac.una.taskprogramll {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens cr.ac.una.taskprogramll to javafx.fxml;
-    exports cr.ac.una.taskprogramll;
-}
