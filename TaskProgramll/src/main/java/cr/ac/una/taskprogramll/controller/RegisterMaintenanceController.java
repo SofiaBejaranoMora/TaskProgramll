52f786e3aac9796e598be28fe4ff717ce7e54964
SofiaBejaranoMora <142953533+SofiaBejaranoMora@users.noreply.github.com>
2025-03-07 23:05:42 -0600

    FileManager creado
    se creo el manejador de archivos, esta terminado solo falta confirmar si funciona

--- TaskProgramll/src/main/java/cr/ac/una/taskprogramll/controller/RegisterMaintenanceController.java
+++ TaskProgramll/src/main/java/cr/ac/una/taskprogramll/controller/RegisterMaintenanceController.java
@@ -4,20 +4,94 @@
  */
 package cr.ac.una.taskprogramll.controller;
 
+import java.awt.Button;
+import java.awt.Label;
+import java.awt.TextField;
+import java.awt.event.ActionEvent;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 
-/**
- * FXML Controller class
- *
- * @author sofia
- */
+
 public class RegisterMaintenanceController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private TextField TxtLftName;
+
+    @FXML
+    private Button btnLftAccept;
+
+    @FXML
+    private Button btnLftCancel;
+
+    @FXML
+    private Button btnLftDelete;
+
+    @FXML
+    private Button btnLftImage;
+
+    @FXML
+    private Button btnLftPhoto;
+
+    @FXML
+    private ComboBox<String> cmbLftEquipment;
+
+    @FXML
+    private ToggleGroup grpFiltro;
+
+    @FXML
+    private Label labLftHead;
+
+    @FXML
+    private RadioButton rbtnLftEquipment;
+
+    @FXML
+    private RadioButton rbtnLftSport;
+
+    @FXML
+    void OnActionBtnLftAccept(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionBtnLftCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionBtnLftDelete(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionBtnLftImage(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionBtnLftPhoto(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionCmbLftEquipment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionRbtnLftEquipment(ActionEvent event) {
+
+    }
+
+    @FXML
+    void OnActionRbtnLftSport(ActionEvent event) {
+
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
