5331e42908336f0890923a1bfaa6c1c14d11f23f
Michi606 <130305415+Michi606@users.noreply.github.com>
2025-03-08 22:49:24 -0600

    Creacion de Clase Torneo
    Se crea la clase torneo

---
+++ TaskProgramll/src/main/java/cr/ac/una/taskprogramll/model/Tourney.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.ac.una.taskprogramll.model;
+
+/**
+ *
+ * @author Michelle Wittingham
+ */
+public class Tourney {
+    private int id;
+    private int time;
+    private Sport sportype;
+    
+}
